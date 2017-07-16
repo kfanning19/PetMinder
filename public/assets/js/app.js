39113adb61a8329a386a03a8f6269365d520a747
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-07-16 18:36:50 -0500

    clean up main

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,5 +1,9 @@
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 50, // Creates a dropdown of 15 years to control year
-	max: [2020,12, 31]
+    max: [2020, 12, 31]
 });
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+$('.collapsible').collapsible();
