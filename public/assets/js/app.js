bbcad3731bf6cd46ce1fbfd4917a3c6041441244
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-07-16 18:18:43 -0500

    petprofile

---
+++ public/assets/js/app.js
@@ -0,0 +1,5 @@
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 50, // Creates a dropdown of 15 years to control year
+	max: [2020,12, 31]
+});
