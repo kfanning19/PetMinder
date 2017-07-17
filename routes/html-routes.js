eb7b7b4fa6d1ce68da55532d2f87b217c9a8117e
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-07-17 06:01:49 -0500

    routes, faq, and about

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -22,53 +22,14 @@ module.exports = function(app) {
             res.render("petProfile", {pet: data})
         })
     });
-
-    //get Pet Activity Tab
-    app.get("/profile/pet/activity/:id", function(req, res) {
-        models.Pet.findById({
-            where: { id: req.params.id },
-            include: [models.User, models.Activity]
-        }).then(function(data) {
-            res.render("activity", {activity: data})
-        })
+    // FAQ page
+    app.get("/faq", function (req, res){
+        res.render("faq");
     });
 
-    //get Pet Health information
-    app.get("/profile/pet/health/:id", function(req, res) {
-        models.Pet.findById({
-            where: { id: req.params.id },
-            include: [models.User, models.Health, models.Medications, models.Illness, models.Weight]
-        }).then(function(data) {
-            res.render("health",{ health: data});
-        });
-    });
-
-    // get Pet Contacts
-    app.get("/profile/pet/contacts/:id", function(req, res) {
-        models.Pet.findById({
-            where: { id: req.params.id },
-            include: [models.Professional]
-        }).then(function(data) {
-            res.render("contacts", {contacts: data});
-        })
-    });
-    // get Pet Diet
-    app.get("/profile/pet/diet/:id", function(req, res) {
-        models.Pet.findById({
-            where: { id: req.params.id },
-            include: [models.Diet]
-        }).then(function(data) {
-            res.render("diet", {diet: data});
-        })
-    });
-    // get Pet Settings 
-    app.get("/profile/pet/settings/:id", function(req, res) {
-        models.Pet.findById({
-            where: { id: req.params.id },
-            include: models.User
-        }).then(function(data) {
-            res.render("settings", {settings: data});
-        });
+    //About page
+    app.get("/about", function(req, res){
+        res.render("about");
     });
 
 
