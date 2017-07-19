70e5d4263496b71c6cdd09fcc4acd62b9ef7681a
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-07-19 05:59:00 -0500

    html

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,3 +7,19 @@ $('.datepicker').pickadate({
 $(".button-collapse").sideNav();
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 $('.collapsible').collapsible();
+
+$('.timepicker').pickatime({
+    default: 'now', // Set default time
+    fromnow: 0, // set default time to * milliseconds from now (using with default = 'now')
+    twelvehour: false, // Use AM/PM or 24-hour format
+    donetext: 'OK', // text for done-button
+    cleartext: 'Clear', // text for clear-button
+    canceltext: 'Cancel', // Text for cancel-button
+    autoclose: false, // automatic close timepicker
+    ampmclickable: true, // make AM PM clickable
+    aftershow: function() {} //Function for after opening timepicker  
+});
+$(document).ready(function() {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
